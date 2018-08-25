4a32cf77a9bbbf42a594c97aee6a2a7260bf44ae
Lanceconan <conan_022@hotmail.com>
2018-08-24 22:13:11 -0300

    ngFor para cargar radio, checkbox, tablas y combobox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,16 @@ export class AppComponent {
   show:boolean = true;
   texto:boolean = true;
   cursos:string[] = ['HTML', 'ANGULAR', 'PHP'];
+  paises:string[] = ['CHILE','ARGENTINA','PERU','BRASIL','VENEZUELA'];
+  generos:string[] = ['HOMBRE','MUJER','OTRO'];
+  estilos:string[] = ['LEER','ESCRIBIR','SUMAR','RESTAR'];
+
+  public test:any = { limite_semana: false };
+
+  animales:Array<any> = [
+    {tipo : 'Perro', nombre: 'Tambito', edad: 13},
+    {tipo : 'Gato', nombre : 'Grenda Pelito', edad : 8},
+    {tipo : 'Hamster', nombre : 'Pikachu', edad : 5}
+  ];
 
 }
